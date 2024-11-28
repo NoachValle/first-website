8421be4292230fe0ae7a93ca55abb149bb0632fc
Noach Valle <noach1977@gmail.com>
2024-11-27 19:24:15 -0800

    This will be a new folder that will contain the basic components like: Header, Content, Footer

---
+++ src/app/Components/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Header = ()=>{
+    return(
+        <header className=' flex justify-evenly items-center w-full h-full '>
+            <h1 className='text-4xl '>First Website</h1>
+            <nav></nav>
+        </header>
+      
+    );
+}
+
+export default Header;
