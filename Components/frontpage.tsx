479df9d14a51d92935a9f4cbb7d77141c25dd22f
Noach Valle <noach1977@gmail.com>
2024-12-11 21:57:56 -0800

    this will be the area that you first see when on this website (will be updated)

---
+++ Components/frontpage.tsx
@@ -0,0 +1,18 @@
+
+const frontpage = ()=>{
+    return (
+    <div className="flex justify-evenly items-center">
+            <div className="">
+      
+                <div>
+                    <img src="https://img.freepik.com/free-vector/cartoon-gamer-room-illustration_52683-60981.jpg"/>
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-vector/cartoon-gamer-room-illustration_52683-60981.jpg"/>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default frontpage
