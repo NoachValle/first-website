45ec253e13e24549f388d625664d4a09f7cc7af9
Noach Valle <noach1977@gmail.com>
2024-12-26 21:32:37 -0800

    this is the main page you will see when on the website(maybe change later)

--- Components/frontpage.tsx
+++ Components/frontpage.tsx
@@ -1,17 +1,46 @@
 
-const frontpage = ()=>{
+const frontpage = () => {
     return (
-    <div className="flex justify-evenly items-center">
-            <div className="">
-      
-                <div>
-                    <img src="https://img.freepik.com/free-vector/cartoon-gamer-room-illustration_52683-60981.jpg"/>
+        <section className="flex items-center flex-col">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white ">
+                            Create an account for First Website
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label className="block mb-2 text-sm font-medium ">Your email</label>
+                                <input type="email" name="email" id="email" className=" text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="name@company.com" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                            <p className="text-sm font-light">
+                                Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                            </p>
+                        </form>
+                    </div>
                 </div>
-                <div>
-                    <img src="https://img.freepik.com/free-vector/cartoon-gamer-room-illustration_52683-60981.jpg"/>
             </div>
-        </div>
-    </div>
+            <div className="flex justify-evenly items-center">
+                <img className="px-4" src="https://img.freepik.com/free-vector/cartoon-gamer-room-illustration_23-2148918173.jpg"/>
+            </div>
+        </section>
     );
 }
 
